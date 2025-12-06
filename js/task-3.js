dfc1370ba4186bc46ea35d4a25ef614ca59a3222
emine-busra <eminebusraacici@hotmail.com>
2025-12-06 17:25:46 +0300

    Edited task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function filterArray(numbers, value) {
   const filteredNumbers = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
     }
   }
   return filteredNumbers;
